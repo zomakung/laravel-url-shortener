d82321a113ad72f2a39f67b439d2b80c567bb084
Chotirod <s.chotirod@gmail.com>
2026-01-13 10:20:35 +0800

    justify header overview page

--- resources/views/admin/overview.blade.php
+++ resources/views/admin/overview.blade.php
@@ -1,13 +1,15 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Admin / Overview') }}
-        </h2>
+        <div class="flex justify-between items-center">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                {{ __('Admin / Overview') }}
+            </h2>
 
-        <a href="{{ route('dashboard') }}"
-            class="text-sm text-gray-600 hover:underline">
-            ← Back to Dashboard
-        </a>
+            <a href="{{ route('dashboard') }}"
+                class="text-sm text-gray-600 hover:underline">
+                ← Back to Dashboard
+            </a>
+        </div>
     </x-slot>
 
     <div class="py-12">
